6189f3f4935075ff42e66459e5a5bab4d7ca6bf0
Patrik E Marjanen <pmarjane@melkki.cs.helsinki.fi>
2010-12-11 00:09:31 +0200

    Added new tests to Elem type

--- test.cpp
+++ test.cpp
@@ -24,5 +24,12 @@ void testElem(HTyo::TestClass& test) {
   // A new element should have the constructed string, and next should point to the 0 pointer address.
   Elem a("Hello World");
 
+  Elem b = a;
+  test(a.get_contents() == b.get_contents(), "Copied elem should have same contents");
+  Elem *c = &(a.get_next());
+  Elem *d = &(b.get_next());
+  test(c == d, "Copied elem should have same next address");
   test(a.get_contents() == "Hello World", "Elem string should be 'Hello World'");
+  Elem *ref = &(a.get_next());
+  test(ref == 0, "Elem next should point to 0 (NULL) in the beginning");
 }
